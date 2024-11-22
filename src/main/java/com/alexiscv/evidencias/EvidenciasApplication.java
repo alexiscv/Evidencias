f17c39b1cc30a4ec6d8307ad18aa89cd6482992b
Alexis Castaño <hogo.jp@gmail.com>
2024-11-22 11:52:31 +0100

    add main and properties

---
+++ src/main/java/com/alexiscv/evidencias/EvidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.alexiscv.evidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidenciasApplication.class, args);
+	}
+
+}
